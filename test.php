e4a4243ce72b2df71d17c259e37114ca4037da2c
echohit <hexiaoyuhit@163.com>
2015-02-10 12:44:19 +0800

    delete the password in the test

--- test.php
+++ test.php
@@ -15,7 +15,7 @@
 	$from = "hexiaoyuhit@163.com";
 	$title = "Hi echo";
 	$content = "This is a test";
-	$to = "hexiaoyuhit@163.com";
+	$to = "test@163.com";
 	$mail = new PHPMailer();
 	$mail->CharSet = $charset; //设置采用gb2312中文编码
 	$mail->IsSMTP(); //设置采用SMTP方式发送邮件
@@ -25,13 +25,13 @@
 	$mail->FromName = "echo"; //设置发件人的姓名
 	$mail->SMTPAuth = true; //设置SMTP是否需要密码验证，true表示需要
 	$mail->Username = $from; //设置发送邮件的邮箱
-	$mail->Password = "echo0829"; //设置邮箱的密码
+	$mail->Password = "haha"; //设置邮箱的密码
 	$mail->Subject = $title; //设置邮件的标题
 	$mail->AltBody = "text/html"; // optional, comment out and test
 	$mail->Body = $content; //设置邮件内容
 	$mail->IsHTML(true); //设置内容是否为html类型
 	$mail->WordWrap = 50; //设置每行的字符数
-	$mail->AddReplyTo("hexiaoyuhit@163.com","名字"); //设置回复的收件人的地址
+	$mail->AddReplyTo("test@163.com","名字"); //设置回复的收件人的地址
 	$mail->AddAddress($to); //设置收件的地址
 	if ($attachment != '') //设置附件
 	{
